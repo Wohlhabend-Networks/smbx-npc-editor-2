b5bb446034061c1fb68ab26b37e2aaf3ea9cd02b
Mike Santiago <miketheripper1@gmail.com>
2014-08-15 19:36:45 -0400

    I have to commit again????
    idk help

---
+++ smbx-npc-editor/ini-editor/ExtendedTreeNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ini_editor
+{
+    public class ExtendedTreeNode : TreeNode
+    {
+        /// <summary>
+        /// The type it is. Specifically for the ini file.
+        /// 0. Section
+        /// 1. Key
+        /// 2. Value
+        /// </summary>
+        public TreeNode node;
+        public int type { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">The name, duh</param>
+        /// <param name="_type">Integer type
+        /// 0. Section
+        /// 1. Key
+        /// 2. Value
+        /// </param>
+        public ExtendedTreeNode(string name, int _type) : base(name)
+        {
+            type = _type;
+        }
+
+        public int GetIntegerType()
+        {
+            return type;
+        }
+    }
+}
+
