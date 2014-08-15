30bdf4193a866f952ad4653b14cc2090dd48cbd9
Mike Santiago <miketheripper1@gmail.com>
2014-08-15 19:29:13 -0400

    Started on INI Editor

---
+++ smbx-npc-editor/ini-editor/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace ini_editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.iniSectionTreeView = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.saveKeyButton = new System.Windows.Forms.Button();
+            this.keyValueTextBox = new System.Windows.Forms.TextBox();
+            this.editKeyLabel = new System.Windows.Forms.Label();
+            this.saveValueButton = new System.Windows.Forms.Button();
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.editValueLabel = new System.Windows.Forms.Label();
+            this.saveSectionButton = new System.Windows.Forms.Button();
+            this.secValueTextBox = new System.Windows.Forms.TextBox();
+            this.editSectionLabel = new System.Windows.Forms.Label();
+            this.addKeyButton = new System.Windows.Forms.Button();
+            this.addSectionButton = new System.Windows.Forms.Button();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.menuItem4 = new System.Windows.Forms.MenuItem();
+            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.iniSectionTreeView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.addKeyButton);
+            this.splitContainer1.Panel2.Controls.Add(this.addSectionButton);
+            this.splitContainer1.Size = new System.Drawing.Size(813, 486);
+            this.splitContainer1.SplitterDistance = 132;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // iniSectionTreeView
+            // 
+            this.iniSectionTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.iniSectionTreeView.Location = new System.Drawing.Point(0, 0);
+            this.iniSectionTreeView.Name = "iniSectionTreeView";
+            this.iniSectionTreeView.Size = new System.Drawing.Size(132, 486);
+            this.iniSectionTreeView.TabIndex = 0;
+            this.iniSectionTreeView.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.iniSectionTreeView_NodeMouseDoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.saveKeyButton);
+            this.groupBox1.Controls.Add(this.keyValueTextBox);
+            this.groupBox1.Controls.Add(this.editKeyLabel);
+            this.groupBox1.Controls.Add(this.saveValueButton);
+            this.groupBox1.Controls.Add(this.valueTextBox);
+            this.groupBox1.Controls.Add(this.editValueLabel);
+            this.groupBox1.Controls.Add(this.saveSectionButton);
+            this.groupBox1.Controls.Add(this.secValueTextBox);
+            this.groupBox1.Controls.Add(this.editSectionLabel);
+            this.groupBox1.Location = new System.Drawing.Point(13, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(652, 432);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Edit Value";
+            // 
+            // saveKeyButton
+            // 
+            this.saveKeyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveKeyButton.Location = new System.Drawing.Point(569, 102);
+            this.saveKeyButton.Name = "saveKeyButton";
+            this.saveKeyButton.Size = new System.Drawing.Size(74, 23);
+            this.saveKeyButton.TabIndex = 8;
+            this.saveKeyButton.Text = "Save";
+            this.saveKeyButton.UseVisualStyleBackColor = true;
+            this.saveKeyButton.Click += new System.EventHandler(this.saveKeyButton_Click);
+            // 
+            // keyValueTextBox
+            // 
+            this.keyValueTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.keyValueTextBox.Location = new System.Drawing.Point(12, 104);
+            this.keyValueTextBox.Name = "keyValueTextBox";
+            this.keyValueTextBox.Size = new System.Drawing.Size(551, 20);
+            this.keyValueTextBox.TabIndex = 6;
+            // 
+            // editKeyLabel
+            // 
+            this.editKeyLabel.AutoSize = true;
+            this.editKeyLabel.Location = new System.Drawing.Point(11, 88);
+            this.editKeyLabel.Name = "editKeyLabel";
+            this.editKeyLabel.Size = new System.Drawing.Size(46, 13);
+            this.editKeyLabel.TabIndex = 7;
+            this.editKeyLabel.Text = "Edit Key";
+            // 
+            // saveValueButton
+            // 
+            this.saveValueButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveValueButton.Location = new System.Drawing.Point(569, 141);
+            this.saveValueButton.Name = "saveValueButton";
+            this.saveValueButton.Size = new System.Drawing.Size(74, 23);
+            this.saveValueButton.TabIndex = 5;
+            this.saveValueButton.Text = "Save";
+            this.saveValueButton.UseVisualStyleBackColor = true;
+            this.saveValueButton.Click += new System.EventHandler(this.saveValueButton_Click);
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.valueTextBox.Location = new System.Drawing.Point(12, 143);
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.Size = new System.Drawing.Size(551, 20);
+            this.valueTextBox.TabIndex = 3;
+            // 
+            // editValueLabel
+            // 
+            this.editValueLabel.AutoSize = true;
+            this.editValueLabel.Location = new System.Drawing.Point(11, 127);
+            this.editValueLabel.Name = "editValueLabel";
+            this.editValueLabel.Size = new System.Drawing.Size(55, 13);
+            this.editValueLabel.TabIndex = 4;
+            this.editValueLabel.Text = "Edit Value";
+            // 
+            // saveSectionButton
+            // 
+            this.saveSectionButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveSectionButton.Location = new System.Drawing.Point(569, 63);
+            this.saveSectionButton.Name = "saveSectionButton";
+            this.saveSectionButton.Size = new System.Drawing.Size(74, 23);
+            this.saveSectionButton.TabIndex = 2;
+            this.saveSectionButton.Text = "Save";
+            this.saveSectionButton.UseVisualStyleBackColor = true;
+            this.saveSectionButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // secValueTextBox
+            // 
+            this.secValueTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.secValueTextBox.Location = new System.Drawing.Point(12, 65);
+            this.secValueTextBox.Name = "secValueTextBox";
+            this.secValueTextBox.Size = new System.Drawing.Size(551, 20);
+            this.secValueTextBox.TabIndex = 0;
+            // 
+            // editSectionLabel
+            // 
+            this.editSectionLabel.AutoSize = true;
+            this.editSectionLabel.Location = new System.Drawing.Point(11, 49);
+            this.editSectionLabel.Name = "editSectionLabel";
+            this.editSectionLabel.Size = new System.Drawing.Size(64, 13);
+            this.editSectionLabel.TabIndex = 1;
+            this.editSectionLabel.Text = "Edit Section";
+            // 
+            // addKeyButton
+            // 
+            this.addKeyButton.Location = new System.Drawing.Point(94, 13);
+            this.addKeyButton.Name = "addKeyButton";
+            this.addKeyButton.Size = new System.Drawing.Size(75, 23);
+            this.addKeyButton.TabIndex = 4;
+            this.addKeyButton.Text = "Add Key";
+            this.addKeyButton.UseVisualStyleBackColor = true;
+            this.addKeyButton.Click += new System.EventHandler(this.addKeyButton_Click);
+            // 
+            // addSectionButton
+            // 
+            this.addSectionButton.Location = new System.Drawing.Point(13, 13);
+            this.addSectionButton.Name = "addSectionButton";
+            this.addSectionButton.Size = new System.Drawing.Size(75, 23);
+            this.addSectionButton.TabIndex = 3;
+            this.addSectionButton.Text = "Add Section";
+            this.addSectionButton.UseVisualStyleBackColor = true;
+            this.addSectionButton.Click += new System.EventHandler(this.addSectionButton_Click);
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1});
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 0;
+            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem2,
+            this.menuItem4,
+            this.menuItem3});
+            this.menuItem1.Text = "&File";
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Index = 0;
+            this.menuItem2.Shortcut = System.Windows.Forms.Shortcut.CtrlO;
+            this.menuItem2.Text = "&Open";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            // 
+            // menuItem4
+            // 
+            this.menuItem4.Index = 1;
+            this.menuItem4.Shortcut = System.Windows.Forms.Shortcut.CtrlS;
+            this.menuItem4.Text = "&Save";
+            this.menuItem4.Click += new System.EventHandler(this.menuItem4_Click);
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.Index = 2;
+            this.menuItem3.Text = "&Exit";
+            this.menuItem3.Click += new System.EventHandler(this.menuItem3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 486);
+            this.Controls.Add(this.splitContainer1);
+            this.DoubleBuffered = true;
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "C# INI Editor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView iniSectionTreeView;
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.Label editSectionLabel;
+        private System.Windows.Forms.TextBox secValueTextBox;
+        private System.Windows.Forms.Button saveSectionButton;
+        private System.Windows.Forms.MenuItem menuItem4;
+        private System.Windows.Forms.Button addKeyButton;
+        private System.Windows.Forms.Button addSectionButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button saveKeyButton;
+        private System.Windows.Forms.TextBox keyValueTextBox;
+        private System.Windows.Forms.Label editKeyLabel;
+        private System.Windows.Forms.Button saveValueButton;
+        private System.Windows.Forms.TextBox valueTextBox;
+        private System.Windows.Forms.Label editValueLabel;
+    }
+}
+
---
+++ smbx-npc-editor/ini-editor/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using IniFileHandler;
+using ns;
+using System.IO;
+using System.Threading;
+
+namespace ini_editor
+{
+    public partial class Form1 : Form
+    {
+        IniFile ini = new IniFile();
+        List<string> sectionsList = new List<string>();
+        List<string> keysList = new List<string>();
+        string parentNodeValue;
+        string nodeKeyValue;
+        //
+        string curFileName;
+        bool changed;
+        bool isSection;
+        int secNumber;
+        int keyNumber;
+        //
+        IniFile.IniSection currentSection;
+        string currentKey;
+        string currentKeyValue;
+        ExtendedTreeNode SELECTEDNODE;
+        //
+
+        public Form1()
+        {
+            Font = SystemFonts.MessageBoxFont;
+            Form1.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+               
+        }
+
+        private void menuItem3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "INI Files (*.ini)|*.ini|All Files (*.*)|*.*";
+            switch (changed)
+            {
+                case(true):
+                    DialogResult dr = MessageBox.Show("You have unsaved changes, would you like to save first?", "Warning", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                    switch (dr)
+                    {
+                        case(DialogResult.Yes):
+                            save();
+                            goto DialogResult;
+                        case(DialogResult.No):
+                            goto DialogResult;
+                        case(DialogResult.Cancel):
+                            goto LolSkipEverything;
+                    }
+                    break;
+                case(false):
+                    break;
+            }
+        DialogResult:
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                curFileName = of.FileName; 
+                this.Cursor = Cursors.WaitCursor;
+                openIniFile();
+            }
+        LolSkipEverything:
+            Console.WriteLine("Skipped");
+        }
+
+        private void iniSectionTreeView_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            ExtendedTreeNode selected = (ExtendedTreeNode)iniSectionTreeView.SelectedNode;
+
+            switch(selected.GetIntegerType())
+            {
+                    //0 is a section, 1 is a key, 2 is a value but 2 is just there as a just in case
+                case(0):
+                    currentSection = ini.GetSection(selected.Text);
+                    SELECTEDNODE = selected;
+                    editSectionLabel.Text = String.Format("Edit Section {0}", currentSection.Name.ToString());
+                    secValueTextBox.Text = currentSection.Name.ToString();
+                    break;
+                case(1):
+                    currentSection = ini.GetSection(selected.Parent.Text);
+                    currentKey = selected.Text;
+                    currentKeyValue = ini.GetKeyValue(currentSection.Name.ToString(), selected.Text);
+                    //
+                    editSectionLabel.Text = String.Format("Edit Section {0}", currentSection.Name.ToString());
+                    secValueTextBox.Text = currentSection.Name.ToString();
+                    //
+                    editKeyLabel.Text = String.Format("Edit Section {0}", currentKey);
+                    keyValueTextBox.Text = currentKey;
+                    //
+                    if (currentKeyValue != null)
+                    {
+                        string value = currentKeyValue;
+                        currentKeyValue = value.Replace("\"", "");
+                        valueTextBox.Text = currentKeyValue;
+                        editValueLabel.Text = String.Format("Edit Value {0}", currentKey);
+                    }
+                    SELECTEDNODE = selected;
+                    break;
+                case(2):
+                    //invalid????
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (SELECTEDNODE.GetIntegerType())
+            {
+                case(0):
+                    ini.RenameSection(SELECTEDNODE.Text, secValueTextBox.Text);
+                    SELECTEDNODE.Text = secValueTextBox.Text;
+                    break;
+                case(1):
+                    MessageBox.Show("Section not selected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                case(2):
+                    MessageBox.Show("Section not selected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+            changed = true;
+        }
+
+        private void menuItem4_Click(object sender, EventArgs e)
+        {
+            save();
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+                switch (changed)
+                {
+                    case (true):
+                        DialogResult dr = MessageBox.Show("You have unsaved changes, would you like to save first?", "Warning", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                        switch (dr)
+                        {
+                            case (DialogResult.Yes):
+                                save();
+                                e.Cancel = false;
+                                break;
+                            case (DialogResult.No):
+                                e.Cancel = false;
+                                Environment.Exit(0);
+                                break;
+                            case (DialogResult.Cancel):
+                                e.Cancel = true;
+                                break;
+                        }
+                        break;
+                    case (false):
+                        Environment.Exit(0);
+                        break;
+                }  
+        }
+
+        private void addSectionButton_Click(object sender, EventArgs e)
+        {
+            ExtendedTreeNode addSection = new ExtendedTreeNode(String.Format("NewSection{0}", secNumber), 0);
+            iniSectionTreeView.Nodes.Add(addSection);
+            ini.AddSection(addSection.Text);
+            secNumber++;
+        }
+
+        private void addKeyButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ExtendedTreeNode selected = (ExtendedTreeNode)iniSectionTreeView.SelectedNode;
+
+                switch (selected.GetIntegerType())
+                {
+                    case (0):
+                        ExtendedTreeNode addKey = new ExtendedTreeNode(String.Format("NewKey{0}", keyNumber), 1);
+                        selected.Nodes.Add(addKey);
+                        IniFile.IniSection selectedSection = ini.GetSection(selected.Text);
+                        selectedSection.AddKey(String.Format("NewKey{0}", keyNumber));
+                        keyNumber++;
+                        break;
+                    case (1):
+                        MessageBox.Show("Please select a section node", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    case (2):
+                        MessageBox.Show("Please select a section node", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please select a section node!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+        private void saveKeyButton_Click(object sender, EventArgs e)
+        {
+            switch (SELECTEDNODE.GetIntegerType())
+            {
+                case (0):
+                    MessageBox.Show("Key not selected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                case (1):
+                    ini.RenameKey(SELECTEDNODE.Parent.Text, SELECTEDNODE.Text, keyValueTextBox.Text);
+                    SELECTEDNODE.Text = keyValueTextBox.Text;
+                    break;
+                case (2):
+                    MessageBox.Show("Key not selected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+            changed = true;
+        }
+
+        private void saveValueButton_Click(object sender, EventArgs e)
+        {
+            switch (SELECTEDNODE.GetIntegerType())
+            {
+                case (0):
+                    MessageBox.Show("[error intensifies]", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                case (1):
+                    ini.SetKeyValue(SELECTEDNODE.Parent.Text, SELECTEDNODE.Text, valueTextBox.Text);
+                    break;
+                case (2):
+                    MessageBox.Show("[error intensifies]", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+            changed = true;
+        }
+
+        #region User Defined Voids
+        void clear()
+        {
+            foreach (TreeNode node in iniSectionTreeView.Nodes)
+            {
+                node.Remove();
+            }
+            changed = false;
+        }
+
+        void exit()
+        {
+            Application.Exit();
+        }
+
+        void save()
+        {
+            if (curFileName != null)
+            {
+                this.Cursor = Cursors.WaitCursor;
+                try
+                {
+                    ini.Save(curFileName);
+                    MessageBox.Show(String.Format("File {0} saved to {1} successfully!", Path.GetFileName(curFileName), Path.GetFullPath(curFileName)),
+                        "Information",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                    changed = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(String.Format("Error while saving file {0}\n\n{1}", curFileName, ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                this.Cursor = Cursors.Arrow;
+            }
+        }
+
+        void openIniFile()
+        {
+            clear();
+            try
+            {
+                iniSectionTreeView.BeginUpdate();
+                ini.Load(curFileName);
+                foreach (IniFile.IniSection s in ini.Sections)
+                {
+                    sectionsList.Add(s.Name);
+                    foreach (IniFile.IniSection.IniKey k in s.Keys)
+                    {
+                        keysList.Add(k.Name);
+                    }
+                }
+
+                foreach (IniFile.IniSection s in ini.Sections)
+                {
+                    ExtendedTreeNode ex = new ExtendedTreeNode(s.Name, 0);
+                    foreach (IniFile.IniSection.IniKey k in s.Keys)
+                    {
+                        ExtendedTreeNode keyy = new ExtendedTreeNode(k.Name, 1);
+                        ex.Nodes.Add(keyy);
+                    }
+                    iniSectionTreeView.Nodes.Add(ex);
+                }
+                iniSectionTreeView.TreeViewNodeSorter = new NumericComparer();
+                iniSectionTreeView.EndUpdate();
+                this.Text = String.Format("C# INI Editor - {0}", Path.GetFileName(curFileName));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Text = "C# INI Editor";
+                curFileName = null;
+            }
+            this.Cursor = Cursors.Arrow;
+        }
+        #endregion
+
+    }
+}
