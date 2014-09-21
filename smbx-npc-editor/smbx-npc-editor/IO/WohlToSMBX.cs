199beebb002deab95de392d0a2709bcecc214717
Mike Santiago <miketheripper1@gmail.com>
2014-09-21 16:38:02 -0400

    Added SMBX to wohl conversion

--- smbx-npc-editor/smbx-npc-editor/IO/WohlToSMBX.cs
+++ smbx-npc-editor/smbx-npc-editor/IO/WohlToSMBX.cs
@@ -18,7 +18,7 @@ namespace smbx_npc_editor.IO
         /// <param name="wohlKey">The key to use (ex: gfx-offset-x)</param>
         /// <param name="npcId">The NPC's ID</param>
         /// <returns></returns>
-        public KeyValuePair<string, string> ConvertToSMBX(IniFile config, string wohlKey, string npcId)
+        public KeyValuePair<string, string> ConvertWohlToSMBX(IniFile config, string wohlKey, string npcId)
         {
             var val = config.ReadValue(npcId, wohlKey);
             switch (wohlKey)
@@ -132,6 +132,82 @@ namespace smbx_npc_editor.IO
             }
             return new KeyValuePair<string, string>("NULL", "NULL");
         }
-
+        //
+        /// <summary>
+        /// Gets the value of the selected SMBX value from Wohl's config
+        /// </summary>
+        /// <param name="config">The instance of the IniFile to use</param>
+        /// <param name="smbxVal">The SMBX value you want to use</param>
+        /// <param name="npcId">The NPC's ID</param>
+        /// <returns></returns>
+        public string ConvertSMBXToWohl(IniFile config, string smbxVal, string npcId)
+        {
+            var val = "filler";
+            switch(smbxVal)
+            {
+                case("gfxoffsetx"):
+                    return val = config.ReadValue(npcId, "gfx-offset-x");
+                case("gfxoffsety"):
+                    return val = config.ReadValue(npcId, "gfx-offset-y");
+                case("gfxheight"):
+                    return val = config.ReadValue(npcId, "gfx-height");
+                case("gfxwidth"):
+                    return val = config.ReadValue(npcId, "gfx-width");
+                case("framestyle"):
+                    return val = config.ReadValue(npcId, "frame-style");
+                case("framespeed"):
+                    throw new NotImplementedException("Conversion for framespeed values not yet supported");
+                case("foreground"):
+                    return val = config.ReadValue(npcId, "foreground");
+                case("speed"):
+                    throw new NotImplementedException("Conversion speed values not yet supported");
+                case("score"):
+                    return val = config.ReadValue(npcId, "score");
+                case("noyoshi"):
+                    return inverseValues(config.ReadValue(npcId, "yoshicaneat"));
+                case("grabside"):
+                    return val = config.ReadValue(npcId, "grab-side");
+                case("grabtop"):
+                    return val = config.ReadValue(npcId, "grab-top");
+                case("nohurt"):
+                    return inverseValues(config.ReadValue(npcId, "hurtplayer"));
+                case("width"):
+                    return val = config.ReadValue(npcId, "fixture-width");
+                case("height"):
+                    return val = config.ReadValue(npcId, "fixture-height");
+                case("npcblock"):
+                    return val = config.ReadValue(npcId, "block-npc");
+                case("npcblocktop"):
+                    return val = config.ReadValue(npcId, "block-npc-top");
+                case ("playerblock"):
+                    return val = config.ReadValue(npcId, "block-player");
+                case ("playerblocktop"):
+                    return val = config.ReadValue(npcId, "block-player-top");
+                case("nogravity"):
+                    return inverseValues(config.ReadValue(npcId, "gravity"));
+                case("nofireball"):
+                    return val = config.ReadValue(npcId, "kill-fireball");
+                case("noiceball"):
+                    return val = config.ReadValue(npcId, "kill-iceball");
+                case("cliffturn"):
+                    return val = config.ReadValue(npcId, "cliffturn");
+                case("jumphurt"):
+                    return inverseValues(config.ReadValue(npcId, "kill-onjump"));
+                case("noblockcollision"):
+                    return inverseValues(config.ReadValue(npcId, "collision-blocks"));
+            }
+            return null;
+        }
+        //
+        string inverseValues(string val)
+        {
+            string returnVal;
+            if (val == "1")
+                return returnVal = "0";
+            else if (val == "0")
+                return returnVal = "1";
+            return null;
+        }
+        //
     }
 }
