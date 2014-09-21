c8c8ac4f06db7c36062405aa2ef0fe9f818dabc4
Wohlstand <admin@wohlnet.ru>
2014-09-22 01:06:20 +0400

    OOps Forgot to open access to values :P

--- smbx-npc-editor/smbx-npc-editor/SpriteHandling/NpcAnimator.cs
+++ smbx-npc-editor/smbx-npc-editor/SpriteHandling/NpcAnimator.cs
@@ -14,254 +14,259 @@ namespace smbx_npc_editor.SpriteHandling
     {
         IniFile npcConfig;
         //    [npc-1]
-            ulong id;
+        public ulong id;
         //    name="Goomba"
-            string name;
+        public string name;
         //    group="Enemy" 		;The sort category
-            string group;
+        public string group;
         //    category="Enemy"		;The sort category
-            string category;
+        public string category;
         //    image="npc-1.gif"		;NPC Image file
-            string image_n;
-            string mask_n;
-            Bitmap image;
-            Bitmap mask;
+        public string image_n;
+        public string mask_n;
+        public Bitmap image;
+        public Bitmap mask;
         //    algorithm="0"			;NPC's alhorytm. Alhoritm have states and events (onDie, onTail, onCollisionWithFlyBlock...)
-            int algorithm;
+        public int algorithm;
             //    ;If algorithm = 0, will using basic parametric alhorythm.
         //    ;Else, get alhorythm from list
         //    default-effect=2		;Spawn effect ID on jump-die
-            ulong effect_1;
+        public ulong effect_1;
         //    shell-effect=4			;effect on kick by shell or other NPC
-            ulong effect_2;
+        public ulong effect_2;
 
         //    ; graphics
-            int gfx_offset_x;
+        public int gfx_offset_x;
         //    gfx-offst-x=0
-            int gfx_offset_y;
+        public int gfx_offset_y;
         //    gfx-offst-y=2
-            int gfx_h;
+        public int gfx_h;
         //    gfx-height-y=32
-            int gfx_w;
+        public int gfx_w;
         //    gfx-width-y=32
 
-            bool custom_physics_to_gfx; //The GFX size defining by physics size in the custom configs
+        public bool custom_physics_to_gfx; //The GFX size defining by physics size in the custom configs
 
-            int grid;
+        public int grid;
         //    grid=32
         //    grid-offset-x=0
-            int grid_offset_x;
+        public int grid_offset_x;
         //    grid-offset-y=0
-            int grid_offset_y;
+        public int grid_offset_y;
 
-            int grid_attach_style; //0 - middle, 1 - left side
+        public int grid_attach_style; //0 - middle, 1 - left side
 
         //    frame-style=0	; (0-2) This option in some alhoritmes can be ignored
-            int framestyle;
+        public int framestyle;
         //    frames=2
-            int frames;
+        public int frames;
         //    frame-speed=128
-            int framespeed;
+        public int framespeed;
         //    foreground=0
-            bool foreground;
+        public bool foreground;
         //    background=0
-            bool background;
+        public bool background;
         //    animation-bidirectional=0
-            bool ani_bidir;
+        public bool ani_bidir;
         //    animation-direction=0
-            bool ani_direct;
+        public bool ani_direct;
 
-            bool ani_directed_direct; //Animation direction will be gotten from NPC's direction
+        public bool ani_directed_direct; //Animation direction will be gotten from NPC's direction
         //    ; for editor
         //    custom-animation=0
-            bool custom_animate;
+        public bool custom_animate;
         //    ; this option useful for non-standart algorithmic sprites (for example, bosses)
 
         //    ;custom-animation-alg=0		; Custom animation algorithm
             // 0 simple frame range, 1 - frame Jump; 2 - custom animation sequances
-            int custom_ani_alg;
+        public int custom_ani_alg;
         //    ;custom-animation-fl=0		; First frame for LEFT
-            int custom_ani_fl;
+        public int custom_ani_fl;
         //    ;custom-animation-el=0		; end frame for LEFT / Jump step
-            int custom_ani_el;
+        public int custom_ani_el;
         //    ;custom-animation-fr=0		; first frame for RIGHT
-            int custom_ani_fr;
+        public int custom_ani_fr;
         //    ;custom-animation-er=0		; end frame for RIGHT / Jump step
-            int custom_ani_er;
+        public int custom_ani_er;
 
-            List<int> frames_left;     //Frame srquence for left
-            List<int> frames_right;    //Frame srquence for right
+        public List<int> frames_left;     //Frame srquence for left
+        public List<int> frames_right;    //Frame srquence for right
 
         //    container=0			; NPC can containing inside other NPC (need enable special option type 2)
-            bool container;
+        public bool container;
 
-            int display_frame;
+        public int display_frame;
 
-            bool no_npc_collions;
+        public bool no_npc_collions;
         //    ; this option disabling collisions in editor with other NPCs, but with NPC's of same ID collisions will be checked
 
         //    ; Special option
         //    have-special=0			; Special NPC's option, what can used by NPC's algorithm
-            bool special_option;
+        public bool special_option;
         //    ;special-name="Cheep-cheep"	; 60
 
-            string special_name;
+        public string special_name;
         //    ;special-type=0			; 61 0 combobox, 1 - spin, 2 - npc-id
-            int special_type;
+        public int special_type;
         //    ;special-combobox-size=3		; 62 quantity of options
-            List<string > special_combobox_opts;
+        //public List<string> special_combobox_opts;
         //    ;special-option-0="Swim"		; 63 set title for 0 option combobox
         //    ;special-option-1="Jump"		; 64 set title for 1 option combobox
         //    ;special-option-2="Projective"	; 65 set title for 2 option combobox
-            int special_spin_min;
+        public int special_spin_min;
         //    ;special-spin-min=0		; 66 milimal value of spin
-            int special_spin_max;
+        public int special_spin_max;
         //    ;special-spin-max=25		; 67 maximal value of spin
-            int special_spin_value_offset;
+        public int special_spin_value_offset;
 
         //    have-special-2=0			; Special NPC's option, what can used by NPC's algorithm
-            bool special_option_2; //Second special option
+        public bool special_option_2; //Second special option
         //    special-2-npc-spin-required
-            List<long > special_2_npc_spin_required;
+        //public List<long> special_2_npc_spin_required;
         //    special-2-npc-box-required
-            List<long > special_2_npc_box_required;
+        //public List<long> special_2_npc_box_required;
 
         //    ;special-2-name="Cheep-cheep"	; 60
-            string special_2_name;
+            //string special_2_name;
         //    ;special-2-type=0			; 61 0 combobox, 1 - spin
-            int special_2_type;
+            //int special_2_type;
         //    ;special-combobox-size=3		; 62 quantity of options
-            List<string > special_2_combobox_opts;
+            //List<string > special_2_combobox_opts;
         //    ;special-option-0="Swim"		; 63 set title for 0 option combobox
         //    ;special-option-1="Jump"		; 64 set title for 1 option combobox
         //    ;special-option-2="Projective"	; 65 set title for 2 option combobox
-            int special_2_spin_min;
+            //int special_2_spin_min;
         //    ;special-2-spin-min=0		; 66 milimal value of spin
-            int special_2_spin_max;
+            //int special_2_spin_max;
         //    ;special-2-spin-max=25		; 67 maximal value of spin
-            int special_2_spin_value_offset;
+            //int special_2_spin_value_offset;
             //special-2-spin-value-offset
 
         //    ;game process
         //    score=2				; Add scores to player (value 0-13)
         //    ; 0, 10, 100, 200, 400, 800, 1000, 2000, 4000, 8000, 1up, 2up, 5up, 3up
-            int score;
+        public int score;
         //    speed=64			; Default movement speed in px/s
-            int speed;
+        public int speed;
         //    moving=1			; NPC simply moving right/left
 
-            bool movement;
+        public bool movement;
         //    scenery=0			; NPC as block
-            bool scenery;
+        public bool scenery;
         //    immortal=0			; NPC Can't be destroy
-            bool immortal;
+        public bool immortal;
         //    yoshicaneat=1			; NPC can be eaten by yoshi
-            bool can_be_eaten;
+        public bool can_be_eaten;
         //    takeble=0			; NPC destroyble on contact with player
-            bool takable;
+        public bool takable;
         //    grab-side=0			; NPC can be grabbed on side
-            bool grab_side;
+        public bool grab_side;
         //    grab-top=0			; NPC can be grabbed on top
-            bool grab_top;
+        public bool grab_top;
         //    grab-any=0			; NPC can be grabbed on any collisions
-            bool grab_any;
+        public bool grab_any;
         //    default-health=1		; NPC's health value
-            int health;
+        public int health;
         //    hurtplayer=1			; Hurt player on contact
-            bool hurt_player;
+        public bool hurt_player;
         //    hurtnpc=0			; Hurt other npc on contact
-            bool hurt_npc;
+        public bool hurt_npc;
 
         //    ;Editor featured
-            string direct_alt_title;
-            string direct_alt_left;
-            string direct_alt_right;
-            bool direct_disable_random;
+            //string direct_alt_title;
+            //string direct_alt_left;
+            //string direct_alt_right;
+            //bool direct_disable_random;
 
         //    allow-bubble=1			; Allow packable into the bubble
-            bool allow_bubble;
+            //bool allow_bubble;
         //    allow-egg=1			; Allow packable into the egg
-            bool allow_egg;
+            //bool allow_egg;
         //    allow-lakitu=1			; Allow packable into the SMW Lakitu
-            bool allow_lakitu;
+            //bool allow_lakitu;
         //    allow-burred=1			; Allow packable under the herb
-            bool allow_buried;
+            //bool allow_buried;
 
 
         //    ; Physics
         //    ; Size of NPC's body (Collision box)
         //    fixture-height=32
-            uint height;
+        public uint height;
         //    fixture-width=32
-            uint width;
+        public uint width;
         //    block-npc=1		; NPC is a solid object for NPC's
-            bool block_npc;
+        public bool block_npc;
         //    block-npc-top=0		; on NPC's top can be stay other NPC's
-            bool block_npc_top;
+        public bool block_npc_top;
         //    block-player=0		; NPC is a solid object for player
-            bool block_player;
+        public bool block_player;
         //    block-player-top=0	; on NPC's top can be stay player
-            bool block_player_top;
+        public bool block_player_top;
         //    collision-blocks=1	; Enable collisions with blocks
-            bool collision_with_blocks;
+        public bool collision_with_blocks;
         //    gravity=1		; Enable gravitation for this NPC
-            bool gravity;
+        public bool gravity;
         //    adhesion=0		; allows to NPC walking on wall and on celling
-            bool adhesion;
+        public bool adhesion;
 
         //    ;Events
         //    deactivate=1		; Deactivate on state offscreen > 4 sec ago
-            bool deactivation;
+        public bool deactivation;
         //    kill-slside=1		; Kill on Slope slide
-            bool kill_slide_slope;
+        public bool kill_slide_slope;
         //    kill-onjump=1		; Kill on jump on NPC's head
-            bool kill_on_jump;
+        public bool kill_on_jump;
         //    kill-bynpc=1		; Kill by contact with other NPC with hurt-npc
         //    ; for example: moving SHELL have "HURT_NPC", and shell kiling ALL NPCs on contact
-            bool kill_by_npc;
+        public bool kill_by_npc;
         //    kill-fireball=1		; kill on collision with NPC, marked as "fireball"
-            bool kill_by_fireball;
+        public bool kill_by_fireball;
         //    kill-iceball=1		; freeze on collision with NPC, marked as "iceball"
-            bool freeze_by_iceball;
+        public bool freeze_by_iceball;
         //    kill-hammer=1		; kill on collision with NPC, marked as "hammer" or "boomerang"
-            bool kill_hammer;
+        public bool kill_hammer;
         //    kill-tail=1		; kill on tail attack
-            bool kill_tail;
+        public bool kill_tail;
         //    kill-spin=1		; kill on spin jump
-            bool kill_by_spinjump;
+        public bool kill_by_spinjump;
         //    kill-statue=1		; kill on tanooki statue fall
-            bool kill_by_statue;
+        public bool kill_by_statue;
         //    kill-with-mounted=1	; kill on jump with mounted items
-            bool kill_by_mounted_item;
+        public bool kill_by_mounted_item;
         //    kill-on-eat=1		; Kill on eat, or transform into other
-            bool kill_on_eat;
+        public bool kill_on_eat;
         //    cliffturn=0		; NPC turns on cliff
-            bool turn_on_cliff_detect;
+        public bool turn_on_cliff_detect;
         //    lava-protection=0	; NPC will not be burn in lava
-            bool lava_protect;
+        public bool lava_protect;
 
             bool is_star; //If this marker was set, this NPC will be markered as "star"
             //Quantity placed NPC's with marker "star" will be save in LVL-file
 
             //Editor defaults
-            bool default_friendly;
-            bool default_friendly_value;
+            //bool default_friendly;
+            //bool default_friendly_value;
 
-            bool default_nomovable;
-            bool default_nomovable_value;
+            //bool default_nomovable;
+            //bool default_nomovable_value;
 
-            bool default_boss;
-            bool default_boss_value;
+            //bool default_boss;
+            //bool default_boss_value;
 
-            bool default_special;
-            long default_special_value;
+            //bool default_special;
+            //long default_special_value;
 
 
-            bool isValid;
+            public bool isValid;
 
-            public obj_npc() { isValid = false; }
+            public obj_npc()
+            {
+                isValid = false;
+                frames_left = new List<int>();
+                frames_right = new List<int>();
+            }
 
-            void init(string configToUse, string npcID)
+            public void init(string configToUse, string npcID)
             {
                 isValid = false;
                 if (!File.Exists(configToUse)) return;
